e30ad307cf5a6c6d26a28b95c66b65fd16053372
Viraj Gavade <vrajgavade17@gmail.com>
2024-09-15 20:30:59 +0530

    Basic App Js Set up completed

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+// app.use('/api/v1/url',UrlRouter)
+
+app.get('/api/v1',(req,res)=>{
+    res.send('hello')
+})
+
+
+app.listen(port,()=>{
+    console.log('Server is Listening on port:-',port)
+})
