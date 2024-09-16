e0f81c8e9bc14941b01fb795a38640c07d57ff2d
Viraj Gavade <vrajgavade17@gmail.com>
2024-09-16 15:16:26 +0530

    "Updated getShortenedUrl function to generate short ID using nanoid, create URL entry in database, and return success or error response."

--- Controllers/url.controllers.js
+++ Controllers/url.controllers.js
@@ -1,8 +1,29 @@
 const URL = require('../Models/models.url')
+const { nanoid } = require('nanoid')
 
-
-const getShortenedUrl = (req,res)=>{
-    res.send('Get the shortened Url')
+const getShortenedUrl = async (req,res)=>{
+    const { url } = req.body
+    if(!url){
+       return  res.status(400).json({error:"Url Is Required!"})
+    }
+    const shortId = nanoid(8)
+    const shortenedURL = await URL.create({
+        shortId:shortId,
+        redirectURL:url,
+        visitHistory:[]
+    })
+    const checkURL = await URL.findById(shortenedURL._id)
+    if(!checkURL){
+        return res.status(501).json(
+            {
+                error:"Something went wrong while creating the short Id!"
+            }
+        )
+    }
+    return res.status(200).json({
+        msg:"Short URL Generated Successfully!",
+        checkURL
+    })
 }
 
 const redirectUrl = (req,res)=>{
