3f0c3de68f67d354ca857f6ebe9016b737c346cf
Viraj Gavade <vrajgavade17@gmail.com>
2024-09-23 13:56:09 +0530

    "Added EJS template engine, updated app.js to use EJS for server-side rendering, and modified package.json and package-lock.json accordingly."

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 require('dotenv').config() //Loading the secrete string
 
 const URL = require('./Models/models.url')//Importing the url model.
-
+const path = require('path')
 const express = require('express')
 const {urlRouter,HealthCheckRouter} = require('./Routes/url.router') //Importing the routers.
 
 const ConnectDB = require('./DataBase/connect')//Importing the connection fucntion.
 
 const app = express()//Creating an express app.
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
 
 const port = process.env.PORT || 8000 //Defing the system port.
 
 app.use(express.json())
-
 app.use('/api/v1/url',urlRouter) //Setting up the rest api.
 
-
+//Server side rendering
+app.get('/test',async (req,res)=>{
+    const allUrls = await URL.find({})
+    res.render('home',{
+        urls:allUrls
+    })
+    // res.end("<h1>This is response</h1>")
+})
 //Functionality to redirect the user to the orignal url with the help of shortned url and update the analytics of the short url
 
-app.get('/:shortId', async (req,res)=>{
+app.get('/url/:shortId', async (req,res)=>{
     const { shortId } = req.params
     console.log(shortId)
     const entry  = await URL.findOneAndUpdate({
